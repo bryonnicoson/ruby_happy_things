733647fdcd28b3dc5b167728cb93df660c84e743
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-08-10 11:07:19 -0500

    sinatra, bubble sort, array search

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'bundler'
 Bundler.require
 
+happy_thoughts = Smile.new
+happy_thoughts.add_smile('Cubs win!')
+happy_thoughts.add_smile('Water is good!')
+happy_thoughts.add_smile('Air is good!')
+happy_thoughts.add_smile('Life is good!')
+
 before '/*' do
 	puts Rainbow('Request ------').send(:aqua)
 	puts Rainbow('Host: ' + request.host + request.path).send(:cyan)
@@ -14,8 +20,21 @@ end
 
 get '/' do
 	{
-		:name => 'indigo montoya',
+		:name => 'inigo montoya',
 		:purpose => 'avenge my father',
 		:quote => 'prepare to die'
 	}.to_json
+end
+
+get '/smiles' do
+
+	{ happy_thoughts => happy_thoughts.get_smiles }.to_json
+	
+end
+
+get '/smiles/:keyword' do
+	@query = params[:keyword]
+	@found = happy_thoughts.find_smile(@query)
+	{ :search_results => @found }.to_json
+	
 end
